895db70bf3657c4035ae6f4a5dfbe067628f7020
THOTA NAGARAJU <naga.thota99@achiever-122.local>
2019-11-19 10:20:45 +0530

    Initial Commit

---
+++ DataSendThruSegues/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DataSendThruSegues
+//
+//  Created by THOTA NAGARAJU on 11/19/19.
+//  Copyright © 2019 THOTA NAGARAJU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
